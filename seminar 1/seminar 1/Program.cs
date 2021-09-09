f1ba3831f7645b9153d787a7781688f527479602
deeereee <rsrthhufer2281337@gmail.com>
2021-09-09 21:05:02 +0300

    Добавили новую ф

--- seminar 1/seminar 1/Program.cs
+++ seminar 1/seminar 1/Program.cs
@@ -6,6 +6,20 @@ namespace seminar_1
     {
         static void Main(string[] args)
         {
+            //PERIMETERANDSQUARE();
+            Meandrirh();
+        }
+        private static void Meandrirh()
+        {
+            Console.WriteLine("Введите число 1: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число 2: ");
+            double b = double.Parse(Console.ReadLine());
+            double mean = (a + b) / 2;
+            Console.WriteLine($"Среднее арифметическое {mean}");
+        }
+        private static void PERIMETERANDSQUARE()
+        {
             Console.WriteLine("Введите сторону квадрата: ");
             int a = int.Parse(Console.ReadLine());
             int P = 4 * a;
